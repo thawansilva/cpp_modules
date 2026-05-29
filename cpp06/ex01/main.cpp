eb7d510115278bffdce78116c952a2022dbf809c
Thawan Silva <twnpsilva@gmail.com>
2026-05-28 22:11:04 -0300

    test: Fix the way program is being tested

--- cpp06/ex01/main.cpp
+++ cpp06/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: thaperei <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/24 12:54:06 by thaperei          #+#    #+#             */
-/*   Updated: 2026/05/24 13:00:15 by thaperei         ###   ########.fr       */
+/*   Updated: 2026/05/28 21:58:10 by thaperei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,11 @@ int main(void)
 {
 	Data data;
 
-	uintptr_t ptr = Serializer::serialize(&data);
-	Data *res = Serializer::deserialize(ptr);
+	uintptr_t serialize_ptr = Serializer::serialize(&data);
+	Data *deserialize_ptr = Serializer::deserialize(serialize_ptr);
 
-	std::cout << &ptr << std::endl;
-	std::cout << &data << std::endl;
-	std::cout << res << std::endl;
+	std::cout << serialize_ptr << std::endl;
+	std::cout << reinterpret_cast<uintptr_t>(deserialize_ptr) << std::endl;
+	std::cout << deserialize_ptr << std::endl;
 	return (0);
 }

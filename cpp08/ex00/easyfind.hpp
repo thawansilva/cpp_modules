d687ab7a37da19d9f9b8adca8a07be7ce070a336
Thawan Silva <twnpsilva@gmail.com>
2026-05-31 18:00:36 -0300

    feat: Finish ex00, implement differents solutions

--- cpp08/ex00/easyfind.hpp
+++ cpp08/ex00/easyfind.hpp
@@ -6,14 +6,33 @@
 /*   By: thaperei <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/31 13:53:49 by thaperei          #+#    #+#             */
-/*   Updated: 2026/05/31 14:07:59 by thaperei         ###   ########.fr       */
+/*   Updated: 2026/05/31 17:58:12 by thaperei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <array>
+#include <vector>
+#include <deque>
 #include <list>
 #include <algorithm>
 
-template< typename T, int>
-void easyfind(T obj, int target)
+//template< typename T >
+//int easyfind(T &container, int target)
+//{
+//	typename T::iterator i;
+//	for (i = container.begin(); i != container.end(); ++i)
+//	{
+//		if (*i == target)
+//			return 1;
+//	}
+//	return	(0);
+//}
+
+template< typename T >
+int easyfind(T &container, int target)
 {
+	typename T::iterator i = std::find(container.begin(), container.end(), target);
+	if (i != container.end())
+		return 1;
+	return	(0);
 }
--- cpp08/ex00/main.cpp
+++ cpp08/ex00/main.cpp
@@ -6,14 +6,53 @@
 /*   By: thaperei <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/31 13:54:29 by thaperei          #+#    #+#             */
-/*   Updated: 2026/05/31 13:59:34 by thaperei         ###   ########.fr       */
+/*   Updated: 2026/05/31 17:56:51 by thaperei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "easyfind.hpp"
+#include <iostream>
 
 int main()
 {
-	
+	// Array container
+	std::cout << "=== ARRAY ===" << std::endl;
+	std::array<int, 10> arr = { 0, 1, 2, 3, 4, 5, 6};
+
+	int array_result = easyfind<std::array<int, 10>>(arr, 3);
+	if (array_result)
+		std::cout << "Number found" << std::endl;
+	else
+		std::cout << "Number not found" << std::endl;
+
+	// Vector container
+	std::cout << "=== VECTOR ===" << std::endl;
+	std::vector<int> vec = { 0, 1, 2, 3, 4, 5, 6};
+
+	int vec_result = easyfind<std::vector<int>>(vec, 10);
+	if (vec_result)
+		std::cout << "Number found" << std::endl;
+	else
+		std::cout << "Number not found" << std::endl;
+
+	// List container
+	std::cout << "=== LIST ===" << std::endl;
+	std::list<int> list = { 0, 1, 2, 3, 4, 5, 6};
+
+	int list_result = easyfind<std::list<int>>(list, 4);
+	if (list_result)
+		std::cout << "Number found" << std::endl;
+	else
+		std::cout << "Number not found" << std::endl;
+
+	// Dequeue container
+	std::cout << "=== DEQUEUE ===" << std::endl;
+	std::deque<int> d = { 0, 1, 2, 3, 4, 5, 6};
+
+	int deque_result = easyfind<std::deque<int>>(d, -1);
+	if (deque_result)
+		std::cout << "Number found" << std::endl;
+	else
+		std::cout << "Number not found" << std::endl;
 	return 0;
 }

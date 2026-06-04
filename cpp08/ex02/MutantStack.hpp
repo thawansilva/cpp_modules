101a7a19672467870e42ba5ae10d6373e7124f6d
Thawan Silva <twnpsilva@gmail.com>
2026-06-04 14:44:54 -0300

    feat: Finish ex02

--- cpp08/ex02/MutantStack.hpp
+++ cpp08/ex02/MutantStack.hpp
@@ -6,24 +6,48 @@
 /*   By: thaperei <thaperei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/03 20:43:42 by thaperei          #+#    #+#             */
-/*   Updated: 2026/06/03 20:43:43 by thaperei         ###   ########.fr       */
+/*   Updated: 2026/06/04 14:37:58 by thaperei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
-
 #include <stack>
 
-// Implement the begin, cbegin, end, cend
-// iterators: regular, const, reverse and const reverse
-template < typename T>
+template < typename T >
 class MutantStack : public std::stack<T>
 {
+
 public:
-	MutantStack();
-	~MutantStack();
-	MutantStack(const MutantStack& src);
-	MutantStack& operator=(const MutantStack& src);
+	MutantStack() {}
+	~MutantStack() {}
+	MutantStack(const MutantStack& src) { this = src; }
+	MutantStack& operator=(const MutantStack& src)
+	{
+		if (this != &src)
+			this = src;
+		return *this;
+	}
+
+	typedef typename std::stack<T>::container_type::iterator iterator;
 
 	//Member Functions
+	iterator	begin()
+	{
+		return (std::stack<T>::c.begin());
+	}
+
+	iterator	rbegin()
+	{
+		return (std::stack<T>::c.rbegin());
+	}
+	
+	iterator	end()
+	{
+		return (std::stack<T>::c.end());
+	}
+
+	iterator	rend()
+	{
+		return (std::stack<T>::c.rend());
+	}
 };
--- cpp08/ex02/main.cpp
+++ cpp08/ex02/main.cpp
@@ -6,13 +6,42 @@
 /*   By: thaperei <thaperei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/03 20:43:53 by thaperei          #+#    #+#             */
-/*   Updated: 2026/06/03 20:43:56 by thaperei         ###   ########.fr       */
+/*   Updated: 2026/06/04 14:26:33 by thaperei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include "MutantStack.hpp"
 
 int main(void)
 {
+	MutantStack<int> mstack;
+
+	mstack.push(5);
+	mstack.push(17);
+
+	std::cout << mstack.top() << std::endl;
+
+	mstack.pop();
+
+	std::cout << mstack.size() << std::endl;
+
+	mstack.push(3);
+	mstack.push(5);
+	mstack.push(737);
+	mstack.push(0);
+
+	MutantStack<int>::iterator it = mstack.begin();
+	MutantStack<int>::iterator ite = mstack.end();
+
+	++it;
+	--it;
+	while (it != ite)
+	{
+		std::cout << *it << std::endl;
+		++it;
+	}
+	std::stack<int> s(mstack);
+	std::cout << s.top() << std::endl;
 	return 0;
 }

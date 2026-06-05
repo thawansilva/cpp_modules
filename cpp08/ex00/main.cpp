025e247a3246606716040c075e53f472799efd2d
Thawan Silva <twnpsilva@gmail.com>
2026-06-05 16:21:12 -0300

    refactor: Add details to message tests

--- cpp08/ex00/main.cpp
+++ cpp08/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: thaperei <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/31 13:54:29 by thaperei          #+#    #+#             */
-/*   Updated: 2026/06/04 19:45:21 by thaperei         ###   ########.fr       */
+/*   Updated: 2026/06/05 16:19:51 by thaperei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,11 +33,11 @@ int main()
 
 	std::cout << "Searching 20..." << std::endl;
 	if (easyfind(vec, 20))
-		std::cout << "Found!" << std::endl;
+		std::cout << "20 Found!" << std::endl;
 
 	std::cout << "Searching 99..." << std::endl;
 	if (easyfind(vec, 99))
-		std::cout << "Found!" << std::endl;
+		std::cout << "99 Found!" << std::endl;
 
 	separator("LIST");
 
@@ -46,8 +46,8 @@ int main()
 	lst.push_back(-20);
 	lst.push_back(-30);
 
-	if (easyfind(vec, -20))
-		std::cout << "Found!" << std::endl;
+	if (easyfind(lst, -20))
+		std::cout << "-20 Found!" << std::endl;
 
 	separator("DEQUE");
 
@@ -58,13 +58,13 @@ int main()
 	dq.push_back(42);
 
 	if (easyfind(dq, 42))
-		std::cout << "Found 42" << std::endl;
+		std::cout << "42 Found!" << std::endl;
 
 	separator("EMPTY CONTAINER");
 
 	std::vector<int> empty;
 
 	if (easyfind(empty, 5))
-		std::cout << "Found!" << std::endl;
+		std::cout << "5 Found!" << std::endl;
 	return 0;
 }
